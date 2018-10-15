896e4cf50b0db95732d7ac2d18167a2db70d1569
lingjun.jlj <jlj_vip98@qq.com>
2018-10-15 15:33:47 +0800

    添加测试类

--- ssm-web/src/test/java/com/ssm/test/ApplicationTest.java
+++ ssm-web/src/test/java/com/ssm/test/ApplicationTest.java
@@ -2,19 +2,14 @@ package com.ssm.test;
 
 import com.ssm.service.CityService;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author: lingjun.jlj
  * @date: 2018/10/15 15:24
  * @description:
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:applicationContext.xml")
-public class ApplicationTest {
+public class ApplicationTest extends BaseTest {
 
     @Autowired
     private CityService cityService;
---
+++ ssm-web/src/test/java/com/ssm/test/BaseTest.java
@@ -0,0 +1,15 @@
+package com.ssm.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author: lingjun.jlj
+ * @date: 2018/10/15 15:32
+ * @description:
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class BaseTest {
+}
