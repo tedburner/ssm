6e59b5183500ee309b7d3fe07e70c33ef46c858b
Lucifer <jlj_vip98@qq.com>
2018-11-05 14:56:04 +0800

    fix

--- ssm-web/src/main/java/com/ssm/security/UserAuthFilter.java
+++ ssm-web/src/main/java/com/ssm/security/UserAuthFilter.java
@@ -11,6 +11,7 @@ import com.ssm.service.cache.JedisSetService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
